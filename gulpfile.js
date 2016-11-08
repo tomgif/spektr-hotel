3d71c55afb8a339ef5a90b83f66b03418dcfd659
tomgif <tomgif@rambler.ru>
2016-11-09 00:37:23 +0300

    Add other

---
+++ gulpfile.js
@@ -0,0 +1,127 @@
+'use strict';
+
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    prefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    rigger = require('gulp-rigger'),
+    cssmin = require('gulp-minify-css'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    rimraf = require('rimraf'),
+    pug = require('gulp-pug'),
+    plumber = require("gulp-plumber"),
+    connect = require('gulp-connect');
+
+var path = {
+    build: {
+        pug: 'dist/',
+        js: 'dist/js/',
+        css: 'dist/css/',
+        img: 'dist/img/',
+        fonts: 'dist/fonts/'
+    },
+    src: {
+        pug: 'src/*.pug',
+        js: 'src/js/main.js',
+        style: 'src/css/main.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    watch: {
+        pug: 'src/**/*.pug',
+        js: 'src/js/**/*.js',
+        style: 'src/css/**/*.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    clean: './dist'
+};
+
+gulp.task('pug:build', function () {
+    gulp.src(path.src.pug)
+        .pipe(plumber())
+        .pipe(rigger())
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest(path.build.pug))
+        .pipe(connect.reload());
+});
+
+gulp.task('js:build', function () {
+    gulp.src(path.src.js)
+        .pipe(plumber())
+        .pipe(rigger())
+        .pipe(uglify())
+        .pipe(gulp.dest(path.build.js))
+        .pipe(connect.reload());
+});
+
+gulp.task('css:build', function () {
+    gulp.src(path.src.style)
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(prefixer())
+        .pipe(cssmin())
+        .pipe(gulp.dest(path.build.css))
+        .pipe(connect.reload());
+});
+
+gulp.task('image:build', function () {
+    gulp.src(path.src.img)
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()],
+            interlaced: true
+        }))
+        .pipe(gulp.dest(path.build.img))
+        .pipe(connect.reload());
+});
+
+gulp.task('fonts:build', function() {
+    gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.build.fonts))
+});
+
+gulp.task('build', [
+    'pug:build',
+    'js:build',
+    'css:build',
+    'fonts:build',
+    'image:build'
+]);
+
+gulp.task('watch', function(){
+    watch([path.watch.pug], function(event, cb) {
+        gulp.start('pug:build');
+    }, ['pug']);
+    watch([path.watch.style], function(event, cb) {
+        gulp.start('css:build');
+    });
+    watch([path.watch.js], function(event, cb) {
+        gulp.start('js:build');
+    });
+    watch([path.watch.img], function(event, cb) {
+        gulp.start('image:build');
+    });
+    watch([path.watch.fonts], function(event, cb) {
+        gulp.start('fonts:build');
+    });
+});
+
+gulp.task('webserver', function () {
+    connect.server({
+        root: [path.clean],
+        port: 9000,
+        livereload: true
+    });
+});
+
+gulp.task('clean', function (cb) {
+    rimraf(path.clean, cb);
+});
+
+gulp.task('default', ['build', 'webserver', 'watch']);
---
+++ init.bat
@@ -0,0 +1,12 @@
+mkdir src && cd src
+mkdir css && mkdir js && mkdir fonts && mkdir img
+cd js
+echo. 2>main.js 
+mkdir partials && cd partials
+echo. 2>app.js
+cd ../../css
+echo. 2>main.scss
+mkdir partials && cd partials
+echo. 2>app.scss
+cd ../../../
+mkdir dist
